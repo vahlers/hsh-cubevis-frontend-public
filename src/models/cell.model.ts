c3d5411e06601e257384def489e84529a31385cc
Darwin Schlenk <darwin.schlenk@stud.hs-hannover.de>
2020-12-10 12:05:43 +0100

    Implement count retrieval (#156)
    * Fixed standard dev and mean retrieval
    
    * Added count values from validation data to model

--- src/models/cell.model.ts
+++ src/models/cell.model.ts
@@ -13,3 +13,19 @@ export type CubeCellModel = {
 	countMean?: number;
 	countStandardDeviation?: number;
 };
+
+export function compareCubeCells(a: CubeCellModel, b: Record<string, number | string | Ip>): boolean {
+	let aKeys = Object.keys(a);
+	const irrelevantKeys = ['anomalyScore', 'count', 'countMean', 'countStandardDeviation'];
+	aKeys = aKeys.filter((key) => !irrelevantKeys.includes(key));
+	const result = aKeys
+		.map((key) => {
+			const aval = a[key];
+			const bval = b[key];
+			if (aval instanceof Ip && bval instanceof Ip) return aval.toString() === bval.toString();
+			else return a[key] === b[key];
+		})
+		.reduce((accu, current) => accu && current);
+
+	return result;
+}
--- src/services/csvRetrieval.service.ts
+++ src/services/csvRetrieval.service.ts
@@ -1,6 +1,6 @@
 import { CellTypes } from '../enums/cellTypes.enum';
 import { DataType } from '../enums/dataType.enum';
-import { CubeCellModel } from '../models/cell.model';
+import { compareCubeCells, CubeCellModel } from '../models/cell.model';
 import { csv } from 'd3';
 import { Ip } from '../models/ip.modell';
 
@@ -153,6 +153,10 @@ export abstract class CsvRetrievalService {
 		return '/data/checkpoints/epoch_0/';
 	}
 
+	protected singleCountFilePath(): string {
+		return '/data/validation_data/epoch_0/batch_0/item_0_sub_item_0/';
+	}
+
 	public abstract getAnomalyData(dimensions: CellTypes[]): Promise<CubeCellModel[]>;
 	public abstract getCounterData(dimensions: CellTypes[]): Promise<CubeCellModel[]>;
 }
@@ -192,15 +196,27 @@ class D3CsvRetrievalService extends CsvRetrievalService {
 	public getCounterData(dimensions: CellTypes[]): Promise<CubeCellModel[]> {
 		// DOes only have two files and I think cell_models is the correct one.
 		const filename = 'cell_models.csv';
+
+		//This loads the file containing Mean and Standard dev
 		const result = csv(this.countFilePath() + filename)
-			.then((data) => {
-				const models: CubeCellModel[] = [];
+			.then(async (data) => {
+				let models: CubeCellModel[] = [];
+				//Filter stars expression
+				const expr = (type: CellTypes) => {
+					if (dimensions.includes(type)) return (x: string) => x !== '*';
+					else return (x: string) => x === '*';
+				};
+
 				data.forEach((row) => {
 					const model: Record<string, unknown> = {}; //Dont know how to initialize types in typescript
+
 					let addToModel = true;
+					for (let i = 0; i < CellTypes.CELLTYPE_CNT; i++) {
+						addToModel = addToModel && expr(i)(row[CsvRetrievalService.dimName(i)]);
+					}
+
 					dimensions.forEach((dim: CellTypes) => {
 						const e = row[CsvRetrievalService.dimName(dim)];
-						if (e === '*') addToModel = false;
 						if (CsvRetrievalService.expectedType(dim) === 'number') {
 							if (e === '?') model[CsvRetrievalService.modelKeyName(dim)] = Infinity;
 							else model[CsvRetrievalService.modelKeyName(dim)] = parseInt(e);
@@ -213,6 +229,30 @@ class D3CsvRetrievalService extends CsvRetrievalService {
 					model.countStandardDeviation = parseFloat(row['count_z_score_standard_deviation']);
 					if (addToModel) models.push(<CubeCellModel>model);
 				});
+
+				//This loads the file that contains the validation count
+				const singleCountData = await csv(this.singleCountFilePath() + this.fileName(dimensions));
+				singleCountData.forEach((row) => {
+					const model: Record<string, string | number | Ip> = {};
+					dimensions.forEach((dim: CellTypes) => {
+						const e = row[CsvRetrievalService.dimName(dim)];
+						if (CsvRetrievalService.expectedType(dim) === 'number') {
+							if (e === '?') model[CsvRetrievalService.modelKeyName(dim)] = Infinity;
+							else model[CsvRetrievalService.modelKeyName(dim)] = parseInt(e);
+						} else if (CsvRetrievalService.expectedType(dim) === 'ip')
+							model[CsvRetrievalService.modelKeyName(dim)] = new Ip(e);
+						else model[CsvRetrievalService.modelKeyName(dim)] = e;
+					});
+
+					//find corresponding cubecell in existing model
+					const result = models.find((cubecell) => compareCubeCells(cubecell, model));
+
+					//Drop results with no corresponding mean and standard dev
+					if (result !== undefined) result.count = parseFloat(row['count'].replace('[', '').replace(']', ''));
+				});
+
+				//Drop results with no corresponding count
+				models = models.filter((cell) => cell.count !== null);
 				return models;
 			})
 			.catch((error) => {
